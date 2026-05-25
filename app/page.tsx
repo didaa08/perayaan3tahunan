0fa15ec9bdd1e19efc416c92480596c25662c7cc
didaa08 <debbyrannga@gmail.com>
2026-05-25 22:21:48 +0700

    fix bg path

--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,7 @@ export default function Home() {
       <div
         className="absolute inset-0 bg-cover"
         style={{
-          backgroundImage: "url('/images/bg.jpeg')",
+          backgroundImage: "url('/bg.jpeg')",
           backgroundPosition: "center 30%",
         }}
       />
